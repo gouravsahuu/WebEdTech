7375d4acafe3ab6fe9d1020ce71f2d3740682c6a
gouravsahuu <gsgourav.sahu@gmail.com>
2023-09-01 19:48:17 +0530

    updated

--- Backend/webedtech/main/views.py
+++ Backend/webedtech/main/views.py
@@ -19,37 +19,37 @@ class InstructorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView)
     queryset = Instructor.objects.all()
     serializer_class = InstructorSerializer
 
-# def instructor_register(request):
-#     if request.method == 'POST':
-#         form = InstructorRegistrationForm(request.POST)
-#         if form.is_valid():
-#             # Check if the email is unique
-#             email = form.cleaned_data['email']
-#             if Instructor.objects.filter(email=email).exists():
-#                 return JsonResponse({'error': 'Email is already registered.'})
+def instructor_register(request):
+    if request.method == 'POST':
+        form = InstructorRegistrationForm(request.POST)
+        if form.is_valid():
+            # Check if the email is unique
+            email = form.cleaned_data['email']
+            if Instructor.objects.filter(email=email).exists():
+                return JsonResponse({'error': 'Email is already registered.'})
             
-#             # Save the new user
-#             instructor = form.save()
-#             return JsonResponse({'message': 'Register Success'})
-#         else:
-#             return JsonResponse({'error': 'Invalid data submitted.'})
+            # Save the new user
+            instructor = form.save()
+            return JsonResponse({'message': 'Register Success'})
+        else:
+            return JsonResponse({'error': 'Invalid data submitted.'})
 
-#     return JsonResponse({'error': 'Invalid request method.'})
+    return JsonResponse({'error': 'Invalid request method.'})
 
-# def instructor_login(request):
-#     if request.method == 'POST':
-#         email = request.POST.get('email')
-#         password = request.POST.get('password')
+def instructor_login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
 
-#         try:
-#             instructor = Instructor.objects.get(email=email)
-#         except Instructor.DoesNotExist:
-#             return JsonResponse({'error': 'User not found.'})
+        try:
+            instructor = Instructor.objects.get(email=email)
+        except Instructor.DoesNotExist:
+            return JsonResponse({'error': 'User not found.'})
 
-#         if instructor.check_password(password):
-#             token, created = Token.objects.get_or_create(user=instructor)
-#             return JsonResponse({'message': 'Login Success', 'token': token.key})
-#         else:
-#             return JsonResponse({'error': 'Invalid password.'})
+        if instructor.check_password(password):
+            token, created = Token.objects.get_or_create(user=instructor)
+            return JsonResponse({'message': 'Login Success', 'token': token.key})
+        else:
+            return JsonResponse({'error': 'Invalid password.'})
 
-#     return JsonResponse({'error': 'Invalid request method.'})
+    return JsonResponse({'error': 'Invalid request method.'})
