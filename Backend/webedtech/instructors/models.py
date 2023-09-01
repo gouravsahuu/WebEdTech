4f1f8167c79cc805fd52075cd17f2574740faf58
gouravsahuu <gsgourav.sahu@gmail.com>
2023-09-01 10:32:12 +0530

    deleted previous files, starting over

--- Backend/webedtech/instructors/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Instructor(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    highest_qualification = models.CharField(max_length=100)
-    experience = models.CharField(max_length=20)
-    password = models.CharField(max_length=100)
-
