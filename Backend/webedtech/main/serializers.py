4e5111d7f46cdaf849f211ed0c7a4a431b95d868
gouravsahuu <gsgourav.sahu@gmail.com>
2023-09-06 11:19:55 +0530

    created serializers and views

---
+++ Backend/webedtech/main/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Student, Instructor, Assignment, Submission
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class InstructorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Instructor
+        fields = '__all__'
+
+class AssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignment
+        fields = '__all__'
+
+class SubmissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Submission
+        fields = '__all__'
--- Backend/webedtech/main/views.py
+++ Backend/webedtech/main/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Student, Instructor, Assignment, Submission
+from .serializers import StudentSerializer, InstructorSerializer, AssignmentSerializer, SubmissionSerializer
 
-# Create your views here.
+class StudentListCreateView(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    lookup_field = 'student_id'
+
+class InstructorListCreateView(generics.ListCreateAPIView):
+    queryset = Instructor.objects.all()
+    serializer_class = InstructorSerializer
+
+class InstructorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Instructor.objects.all()
+    serializer_class = InstructorSerializer
+    lookup_field = 'instructor_id'
+
+class AssignmentListCreateView(generics.ListCreateAPIView):
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentSerializer
+
+class AssignmentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Assignment.objects.all()
+    serializer_class = AssignmentSerializer
+    lookup_field = 'assignment_id'
+
+class SubmissionCreateView(generics.CreateAPIView):
+    queryset = Submission.objects.all()
+    serializer_class = SubmissionSerializer
+
+class SubmissionDetailView(generics.RetrieveAPIView):
+    queryset = Submission.objects.all()
+    serializer_class = SubmissionSerializer
+    lookup_field = 'submission_id'
